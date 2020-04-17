1d40f6aef52face1186709798cd1dfac0a56d3c6
William Savage <williamsavage@Williams-Air.fios-router.home>
2020-04-16 23:51:51 -0400

    second assessment

---
+++ Module-2_Assessment/game.js
@@ -0,0 +1,47 @@
+// Create an array of words
+var words = [
+  "javascript",
+  "python",
+  "MATLAB",
+  "Java",
+  "Fortran",
+  "programming",
+  "loops",
+  "variable",
+  "Boolean",
+  "conditional",
+  "function",
+  "constant",
+  "output"
+];
+  for (var z=0; z<words.length; z++) {
+  var word = words[Math.floor(Math.random() * words.length)];
+  answerArray=[];
+  for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+    document.write(answerArray)
+  }
+  
+  var remainingLetters = word.length;
+
+  var i=0;
+  
+  while (remainingLetters > 0) {
+    var guess = prompt('Guess a letter, or click Cancel to stop playing')
+
+  
+  for (var j = 0; j < word.length; j++) {
+    if (word[j] === guess) {
+      answerArray[j] = guess; remainingLetters--;
+    }
+    else
+      break
+  }
+  document.write(guess[j])
+  i++
+  }
+  
+  
+  alert(answerArray.join(" "))
+  alert("Good job! The answer was " + word)
+}
